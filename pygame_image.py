c2793d13212ebf48c15db7c881f99df8984308b2
sou <c0a230953c@edu.teu.ac.jp>
2024-09-24 16:27:05 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -19,12 +19,21 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg2_img,[x+1600,0])
         screen.blit(bg_img,[x+3200,0])
         screen.blit(bg2_img,[x+4800,0])
-        screen.blit(kk_img,[300,200])
+        screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
